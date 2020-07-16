3cef2993d256fd43da6a41c15976d66d934e365b
Joao Matos <joao.matos@elecnor.es>
2020-07-16 17:08:52 +0000

    EOEPCA-127 Add resource blueprint; add base flask app

--- src/main.py
+++ src/main.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
+from flask import Flask
+from random import choice
+from string import ascii_lowercase
 
-def main():
-    pass
+from resources.resources import resource_bp
+from config import config_parser
 
-if __name__ == "__main__":
-    main()
+config = config_parser.load_config()
+
+app = Flask(__name__)
+app.secret_key = ''.join(choice(ascii_lowercase) for i in range(30)) # Random key
+
+# Register api blueprints (module endpoints)
+app.register_blueprint(resource_bp, url_prefix="/resource")
+
+
+app.run(
+    debug=config["debug_mode"],
+    port=config["port"],
+    host=config["host"]
+)
