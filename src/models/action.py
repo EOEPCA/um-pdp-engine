02641fb487d47853e766c87404234e51ec26615b
Joao Matos <joao.matos@elecnor.es>
2020-07-16 12:11:30 +0000

    EOEPCA-127 Add model classes for XACML requests

---
+++ src/models/action.py
@@ -0,0 +1,6 @@
+from attribute import Attribute
+
+class Action(Attribute):
+
+    def add_attribute(self, attribute_id, value):
+        super().add_attribute(attribute_id, value)
---
+++ src/models/resource.py
@@ -0,0 +1,6 @@
+from attribute import Attribute
+
+class Resource(Attribute):
+
+    def add_attribute(self, attribute_id, value, data_type, include_in_result):
+        super().add_attribute(attribute_id, value, data_type, include_in_result)
