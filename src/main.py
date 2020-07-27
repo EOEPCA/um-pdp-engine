c3e9c8dff8ab13ce49114097a379552357ddf516
mamuniz <mamuniz@elecnor.es>
2020-07-27 11:39:41 +0200

    EOEPCA-127: #comment Removed insert policies from main file

--- src/main.py
+++ src/main.py
@@ -6,20 +6,6 @@ import json
 
 from policies.policies import policy_bp
 from config import config_parser
-#from policy_storage.py import Policy_Storage
-
-#file_policy_1 = "standards/policy_template_ownership.json"
-#file_policy_2 = "standards/policy_template_access_list.json"
-
-#mongo = Policy_Storage()
-
-#with open(file_policy_1) as json_file_1:
-#    data1 = json.load(json_file_1)
-#    mongo.insert_policy(data1)
-
-#with open(file_policy_2) as json_file_2:
-#    data2 = json.load(json_file_2)
-#    mongo.insert_policy(data2)
 
 config = config_parser.load_config()
 
