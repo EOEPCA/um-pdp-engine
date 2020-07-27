acd3bf2b99a88e862fef5d478820c1d26b066188
mamuniz <mamuniz@elecnor.es>
2020-07-27 17:08:52 +0200

    EOEPCA-127: #comment Refactor parser policies for mongo

--- src/policies/policies_operations.py
+++ src/policies/policies_operations.py
@@ -4,31 +4,25 @@ import json
 def validate_access_policies(resource_id, user_name):
     #mongo = Policy_Storage()
     #data = mongo.get_policy_from_resource_id(str(resource_id))
-
-    #---------------- Once the mongo is used, comment/remove the whole part of using the json file --------------- 
-    #file = "standards/policy_template_ownership.json"
-    file = "standards/policy_template_access_list.json"
-
-    with open(file) as json_file:
-        data = json.load(json_file)
-    # ------------------------------------------------------------------------------------------------------------
+    
     operations = ['AND', 'OR']
 
-    if data['resource_id'] == resource_id:
-        for operation in operations:
-            if operation in data['rules'][0].keys():
-                exist_operation = operation
-                break
+    if isinstance(data, list):
+        for i in range(0, len(data)):
+            if data[i]['config']['resource_id'] == resource_id:
+                for operation in operations:
+                    if operation in data[i]['config']['rules'][0].keys():
+                        exist_operation = operation
+                        break
 
-        if len(data['rules'][0][exist_operation]) == 1:
-            if data['rules'][0][exist_operation][0]['EQUAL']['user_name'] == user_name:
-                return True
-        else:
-            for i in range(0, len(data['rules'][0][exist_operation])):
-               if data['rules'][0][exist_operation][i]['EQUAL']['user_name'] == user_name:
-                   return True
+                if len(data[i]['config']['rules'][0][exist_operation]) == 1:
+                    if data[i]['config']['rules'][0][exist_operation][0]['EQUAL']['user_name'] == user_name:
+                        return True
+                else:
+                    for j in range(0, len(data[i]['config']['rules'][0][exist_operation])):
+                        if data[i]['config']['rules'][0][exist_operation][j]['EQUAL']['user_name'] == user_name:
+                            return True
     return False
-
 
 
 
