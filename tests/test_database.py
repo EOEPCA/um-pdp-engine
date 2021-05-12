3df7f35c860b0e7f966f0e01520831c8c8ab808a
AlvaroVillanueva <alvaro.villanueva@deimos-space.com>
2021-05-12 11:26:56 +0000

    fixing relative path ghactionss

--- tests/test_database.py
+++ tests/test_database.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 import sys
 sys.path.append('../src/')
 print(sys.path)
-from custom_mongo import Mongo_Handler
+from ..src.custom_mongo import Mongo_Handler
 import unittest
 import mock
 
