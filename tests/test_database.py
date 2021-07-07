d99c2a75d2a7d488663be885371ea5a44cec9a4c
AlvaroVillanueva <alvaro.villanueva@deimos-space.com>
2021-07-07 16:19:36 +0000

    updated test database path

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,12 +1,12 @@
 import pymongo
 from pymongo import MongoClient
-from src.custom_mongo import Mongo_Handler
-import unittest
-import mock
-import os
 sys.path.insert(0, '../src')
 dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(dir_path+'/../src/')
+from custom_mongo import Mongo_Handler
+import unittest
+import mock
+import os
 
 def mocked_mongo(*args, **kwargs):
     class MockResponse:
